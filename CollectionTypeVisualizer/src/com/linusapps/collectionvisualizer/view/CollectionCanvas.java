d051abb6369991e6ef124b443735f060b0a566d6
linus <linus@DESKTOP-FKL2R0L>
2016-08-27 20:43:14 +1200

    Added classes and src.

---
+++ CollectionTypeVisualizer/src/com/linusapps/collectionvisualizer/view/CollectionCanvas.java
@@ -0,0 +1,7 @@
+package com.linusapps.collectionvisualizer.view;
+
+import javax.swing.JPanel;
+
+public class CollectionCanvas extends JPanel{
+
+}
---
+++ CollectionTypeVisualizer/src/com/linusapps/collectionvisualizer/view/CollectionFrame.java
@@ -0,0 +1,10 @@
+package com.linusapps.collectionvisualizer.view;
+import javax.swing.JFrame;
+/**
+ * The class that represents the frame.
+ * @author linus
+ *
+ */
+public class CollectionFrame extends JFrame{
+
+}
